365f2678fe32d9b9307a665a0a4d4627445e9c4f
LazY <darshitmistry2@gmail.com>
2024-01-20 19:56:20 +0530

    style:navbar and menu section

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,11 +1,24 @@
-import CurrentUser from "./current-user"
+import { Layout, Space } from "antd";
+import CurrentUser from "./current-user";
 
 const Header = () => {
-  return (
-    <div>
-        <CurrentUser/> 
-    </div>
-  )
+const haderStyles : React.CSSProperties = {
+  background: "#fff",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "flex-end",
+  padding: "0 24px",
+  position: "sticky",
+  top: 0,
+  zIndex: 999,
 }
+  return (
+    <Layout.Header style={haderStyles}>
+      <Space align="center" size="middle">
+        <CurrentUser />
+      </Space>
+    </Layout.Header>
+  );
+};
 
-export default Header
+export default Header;
