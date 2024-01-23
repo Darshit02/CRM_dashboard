db0e2cf665fe7530f1fdb8a152cec8f2f62e78c8
LazY <darshitmistry2@gmail.com>
2024-01-23 12:17:45 +0530

    feat(home): state number card

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,38 +1,78 @@
-import { DealsChart, UpcomingEvents } from "@/components"
-import { Col, Row } from "antd"
-
+import {
+  DashboardTotalCountCard,
+  DealsChart,
+  UpcomingEvents,
+} from "@/components";
+import { DASHBOARD_TOTAL_COUNTS_QUERY } from "@/graphql/queries";
+import { DashboardTotalCountsQuery } from "@/graphql/types";
+import { useCustom } from "@refinedev/core";
+import { Col, Row } from "antd";
 
 export const Home = () => {
+  const { data, isLoading } = useCustom<DashboardTotalCountsQuery>({
+    url: "",
+    method: "get",
+    meta: {
+      gqlQuery: DASHBOARD_TOTAL_COUNTS_QUERY,
+    },
+  });
   return (
     <div>
-      <Row 
-      gutter={[32 , 32]}
-      style={{
-        marginTop: '32px',
-      }}
-      >
-        <Col 
-        xs={24}
-        sm={24}
-        xl={8}
-        style={{
-          height: '460px',
-        }}
-        >
-          <UpcomingEvents/>
+      {/* Total Count card */}
+      <Row gutter={[32, 32]}>
+        <Col xs={24} sm={24} xl={8}>
+          <DashboardTotalCountCard
+            resource="companies"
+            isLoading={isLoading}
+            totalCount={data?.data.companies.totalCount}
+          />
         </Col>
-        <Col 
-        xs={24}
-        sm={24}
-        xl={8}
+
+        <Col xs={24} sm={24} xl={8}>
+          <DashboardTotalCountCard
+            resource="contacts"
+            isLoading={isLoading}
+            totalCount={data?.data.contacts.totalCount}
+          />
+        </Col>
+
+        <Col xs={24} sm={24} xl={8}>
+          <DashboardTotalCountCard
+            resource="deals"
+            isLoading={isLoading}
+            totalCount={data?.data.deals.totalCount}
+          />
+        </Col>
+      </Row>
+      {/* Upcoming Events*/}
+      <Row
+        gutter={[32, 32]}
         style={{
-          height: '460px',
+          marginTop: "32px",
         }}
+      >
+        <Col
+          xs={24}
+          sm={24}
+          xl={8}
+          style={{
+            height: "460px",
+          }}
         >
-          <DealsChart/>
+          <UpcomingEvents />
+        </Col>
+        {/* Deals Chart  */}
+        <Col
+          xs={24}
+          sm={24}
+          xl={16}
+          style={{
+            height: "460px",
+          }}
+        >
+          <DealsChart />
         </Col>
       </Row>
     </div>
-  )
-}
-
+  );
+};
